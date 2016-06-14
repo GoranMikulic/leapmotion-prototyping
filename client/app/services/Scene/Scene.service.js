099cfdeab42f8580c4bb971288a6d1a090ccc423
Goran Mikulic <mikulicg@gmail.com>
2016-06-14 12:38:44 +0200

    defined boxes

--- client/app/services/Scene/Scene.service.js
+++ client/app/services/Scene/Scene.service.js
@@ -3,13 +3,16 @@
 angular.module('cooperationprototypingApp')
   .service('SceneModel', function() {
 
-    function initMeshBox(x, y, z, rotationX) {
+    function initMeshBox(x, y, z, width, height, depth, rotationX) {
       // ground box
-      var geometry = new THREE.BoxGeometry(500, 2, 500);
+      var geometry = new THREE.BoxGeometry(width, height, depth);
       //var geometry = new THREE.CubeGeometry(100, 100, 2, 10, 10);
       //var material = new THREE.MeshNormalMaterial();
       var material = Physijs.createMaterial(
-        new THREE.MeshNormalMaterial({ transparent: true, opacity: 0.4 }),
+        new THREE.MeshNormalMaterial({
+          transparent: true,
+          opacity: 0.4
+        }),
         .4,
         .8
       );
@@ -18,7 +21,7 @@ angular.module('cooperationprototypingApp')
       var mesh = new Physijs.BoxMesh(geometry, material, 0);
 
       mesh.position.set(x, y, z);
-      if(rotationX) {
+      if (rotationX) {
         mesh.rotation.x = rotationX;
       }
 
@@ -54,6 +57,8 @@ angular.module('cooperationprototypingApp')
 
       var camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 5000);
       camera.position.set(500, 500, 500);
+
+
       var controls = new THREE.TrackballControls(camera, renderer.domElement);
       var scene = new Physijs.Scene();
       scene.setGravity(new THREE.Vector3(0, -200, 0));
@@ -61,18 +66,19 @@ angular.module('cooperationprototypingApp')
         scene.simulate(undefined, 2);
       });
 
-      var ground = initMeshBox(0, 50, 0);
+      var ground = initMeshBox(0, 50, 0, 500, 2, 500);
       scene.add(ground);
       //ground = new THREE.GridHelper(250, 10);
       //scene.add(ground);
 
       // build walls
-      var top = initMeshBox(0, 400, 0);
+
+      var top = initMeshBox(0, 400, 0, 500, 2, 500);
       scene.add(top);
       var nintyDegreeRotation = Math.PI / 2;
-      var wall1 = initMeshBox(0, 150, 250, nintyDegreeRotation);
+      var wall1 = initMeshBox(0, 225, 250, 500, 2, 350, nintyDegreeRotation);
       scene.add(wall1);
-      var wall2 = initMeshBox(0, 150, -250, nintyDegreeRotation);
+      var wall2 = initMeshBox(0, 225, -250, 500, 2, 350, nintyDegreeRotation);
       scene.add(wall2);
 
       // axes
