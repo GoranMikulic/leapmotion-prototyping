d55cdc4d0b6ed8430d3b8761b1628aefd49d7c68
Goran Mikulic <mikulicg@gmail.com>
2016-06-18 15:54:03 +0200

    Viewing and removing hand properly

--- client/app/services/Fingers/Fingers.service.js
+++ client/app/services/Fingers/Fingers.service.js
@@ -1,29 +1,36 @@
 'use strict';
 
 angular.module('cooperationprototypingApp')
-  .service('Fingers', function () {
+  .service('Fingers', function() {
 
-    var Fingerling = function(scene, fingerDataViewerId, xposition) {
+    var Fingerling = function(scene, fingerDataViewerId, xposition, name) {
 
       var fingerling = this;
 
-      var tip = addPhalange(scene);
-      var dip = addPhalange(scene);
-      var pip = addPhalange(scene);
-      var mcp = addPhalange(scene);
-      var carp = addPhalange(scene);
+      var handName = name;
+      var fingersScene = scene;
+      var tip = addPhalange(scene, 'tip' + name);
+      var dip = addPhalange(scene, 'dip' + name);
+      var pip = addPhalange(scene, 'pip' + name);
+      var mcp = addPhalange(scene, 'mcp' + name);
+      var carp = addPhalange(scene, 'carp' + name);
 
       fingerling.outputData = function(index, finger) {
-        tip.position.set(xposition, finger.tipPosition[1], finger.tipPosition[2]);
-        cancelVelocity(tip);
-        dip.position.set(xposition, finger.dipPosition[1], finger.dipPosition[2]);
-        cancelVelocity(dip);
-        pip.position.set(xposition, finger.pipPosition[1], finger.pipPosition[2]);
-        cancelVelocity(pip);
-        mcp.position.set(xposition, finger.mcpPosition[1], finger.mcpPosition[2]);
-        cancelVelocity(mcp);
-        carp.position.set(xposition, finger.carpPosition[1], finger.carpPosition[2]);
-        cancelVelocity(carp);
+        tip.position.set(xposition + finger.tipPosition[0], finger.tipPosition[1], finger.tipPosition[2]);
+        //tip.position.set(xposition, finger.tipPosition[1], finger.tipPosition[2]);
+        //cancelVelocity(tip);
+        dip.position.set(xposition + finger.dipPosition[0], finger.dipPosition[1], finger.dipPosition[2]);
+        //dip.position.set(xposition, finger.dipPosition[1], finger.dipPosition[2]);
+        //cancelVelocity(dip);
+        pip.position.set(xposition + finger.pipPosition[0], finger.pipPosition[1], finger.pipPosition[2]);
+        //pip.position.set(xposition, finger.pipPosition[1], finger.pipPosition[2]);
+        //cancelVelocity(pip);
+        mcp.position.set(xposition + finger.mcpPosition[0], finger.mcpPosition[1], finger.mcpPosition[2]);
+        //mcp.position.set(xposition, finger.mcpPosition[1], finger.mcpPosition[2]);
+        //cancelVelocity(mcp);
+        carp.position.set(xposition + finger.carpPosition[0], finger.carpPosition[1], finger.carpPosition[2]);
+        //carp.position.set(xposition, finger.carpPosition[1], finger.carpPosition[2]);
+        //cancelVelocity(carp);
 
         updatePhalange(tip, dip);
         updatePhalange(dip, pip);
@@ -38,23 +45,42 @@ angular.module('cooperationprototypingApp')
 
     };
 
-    function cancelVelocity(threeObject) {
-      //threeObject.__dirtyPosition = true;
-      //threeObject.setLinearVelocity(new THREE.Vector3(0, 0, 0));
-      //threeObject.setAngularVelocity(new THREE.Vector3(0, 0, 0));
-    }
-
-    this.build = function(scene, fingerDataViewerId, xposition) {
-        var finger = new Fingerling(scene, fingerDataViewerId, xposition);
-        return finger;
+    this.build = function(scene, fingerDataViewerId, xposition, name) {
+      var finger = new Fingerling(scene, fingerDataViewerId, xposition, name);
+      return finger;
     };
 
+    this.hideFingerModel = function(handNameId, fingersScene) {
+      changeVisibility(false, handNameId, fingersScene);
+    };
 
-    function addPhalange(scene) {
+    this.viewFingerModel = function(handNameId, fingersScene) {
+      changeVisibility(true, handNameId, fingersScene);
+    };
+
+    function changeVisibility(visibility, handNameId, fingersScene) {
+      var tipObj = fingersScene.getObjectByName('tip' + handNameId);
+      tipObj.visible = visibility;
+
+      var dipObj = fingersScene.getObjectByName('dip' + handNameId);
+      dipObj.visible = visibility;
+
+      var pipObj = fingersScene.getObjectByName('pip' + handNameId);
+      pipObj.visible = visibility;
+
+      var mcpObj = fingersScene.getObjectByName('mcp' + handNameId);
+      mcpObj.visible = visibility;
+
+      var carpObj = fingersScene.getObjectByName('carp' + handNameId);
+      carpObj.visible = visibility;
+    }
+
+    function addPhalange(scene, name) {
 
       var geometry = new THREE.BoxGeometry(20, 20, 1);
       var material = new THREE.MeshNormalMaterial();
       var phalange = new THREE.Mesh(geometry, material, 100);
+      phalange.name = name;
 
       scene.add(phalange);
       return phalange;
--- client/app/services/Hand/Hand.service.js
+++ client/app/services/Hand/Hand.service.js
@@ -13,7 +13,7 @@ angular.module('cooperationprototypingApp')
       //var parent = new Three.Mesh(new THREE.CubeGeometry(0, 0, 0), new THREE.MeshBasicMaterial({ color: 0x888888 }));
 
       var isHostHand = isHost;
-      var geometry = new THREE.BoxGeometry(10, 100, 150);
+      var geometry = new THREE.BoxGeometry(100, 10, 150);
       var material = new THREE.MeshNormalMaterial();
       var handBox = new Physijs.BoxMesh(geometry, material, 1);
       handBox.name = index;
@@ -21,9 +21,9 @@ angular.module('cooperationprototypingApp')
 
       var xposition;
       if(isHostHand) {
-        xposition = 200;
+        xposition = 250;
       } else {
-        xposition = -200;
+        xposition = -250;
       }
 
       scene.add(handBox);
@@ -31,11 +31,12 @@ angular.module('cooperationprototypingApp')
       hand.outputData = function(index, hand) {
 
         handBox.position.set(xposition, hand.stabilizedPalmPosition[1], hand.stabilizedPalmPosition[2]);
-        //handBox.rotation.set(hand.pitch, -hand.yaw, hand.roll);
+        handBox.rotation.set(hand.pitch, -hand.yaw, hand.roll);
         cancelVelocity(handBox);
 
         hand.fingers.forEach(function(finger, index) {
-          var fingerling = (fingerlings[index] || (fingerlings[index] = Fingers.build(scene, fingerData, xposition)));
+          console.log("finger index" , index);
+          var fingerling = (fingerlings[index] || (fingerlings[index] = Fingers.build(scene, fingerData, xposition, handBox.name + index)));
           fingerling.outputData(index, finger);
         });
         //scene.add(parent);
@@ -46,6 +47,7 @@ angular.module('cooperationprototypingApp')
 
     function cancelVelocity(threeObject) {
       threeObject.__dirtyPosition = true;
+      threeObject.__dirtyRotation = true;
       threeObject.setLinearVelocity(new THREE.Vector3(0, 0, 0));
       threeObject.setAngularVelocity(new THREE.Vector3(0, 0, 0));
     }
@@ -54,6 +56,16 @@ angular.module('cooperationprototypingApp')
       angular.forEach(thisClientHands, function(handName) {
         var selectedObject = scene.getObjectByName(handName);
         selectedObject.visible = false;
+
+        for(var i = 0; i < 5; i++) {
+            Fingers.hideFingerModel(handName + i, scene);
+        }
+      });
+    }
+
+    function hideFingers (fingerlings) {
+      angular.forEach(fingerlings, function(finger) {
+        finger.hideFingerModel();
       });
     }
 
@@ -61,6 +73,10 @@ angular.module('cooperationprototypingApp')
       angular.forEach(thisClientHands, function(handName) {
         var selectedObject = scene.getObjectByName(handName);
         selectedObject.visible = true;
+
+        for(var i = 0; i < 5; i++) {
+            Fingers.viewFingerModel(handName + i, scene);
+        }
       });
     }
 
